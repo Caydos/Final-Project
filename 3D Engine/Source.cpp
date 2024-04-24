942001cf897064dd3e42777e175e91ee95a10862
Tobuxi <Etudiant5@DELL17-24>
2024-04-24 16:30:40 +0200

    correct room

--- 3D Engine/Source.cpp
+++ 3D Engine/Source.cpp
@@ -95,7 +95,7 @@ int main()
 
 	gameData.gameStates[TOOLS] = &Tools::Tick;
 	gameData.gameStates[GAME] = &Scritping::Tick;
-	gameData.gameState = GAME;
+	gameData.gameState = TOOLS;
 
 #ifdef _DEBUG
 	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
