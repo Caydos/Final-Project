8ed8cfb7be625b7e6aa6033e5f68cf2dcd55dee0
Tobuxi <Etudiant5@DELL17-24>
2024-04-24 13:34:13 +0200

    update

--- 3D Engine/Props.cpp
+++ 3D Engine/Props.cpp
@@ -10,10 +10,12 @@ Map::ManagmentTextProps Props::TextChunck(int _stageType)
 	{
 		//text.room.push_back("../Sets/HOSPITAL/Map/HSP_PlayRoomDodu.json");
 		//text.room.push_back("../Sets/HOSPITAL/Map/HSP_PlayRoomDodu.json");
-		text.room.push_back("../Sets/HOSPITAL/Map/HSP_WaitingRoom.json");
+		//text.room.push_back("../Sets/HOSPITAL/Map/HSP_WaitingRoom.json");
 		text.room.push_back("../Sets/HOSPITAL/Map/HSP_WeirdRoomChair.json");
 		text.room.push_back("../Sets/HOSPITAL/Map/HSP_PlayRoom.json");
-		text.room.push_back("../Sets/HOSPITAL/Map/HSP_GiantBedRoom.json");
+		//text.room.push_back("../Sets/HOSPITAL/Map/HSP_GiantBedRoom.json");
+		text.room.push_back("../Sets/HOSPITAL/Map/HSP_ReceptionDesk.json");
+		text.room.push_back("../Sets/HOSPITAL/Map/HSP_HOSPITALEXIT.json");
 
 	}
 	if (_stageType == Map::LIBRARY)
